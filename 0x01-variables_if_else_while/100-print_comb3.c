62b6d20d18284c66d8920fae711a602181368e89
P-a-u-l-a <tengeyapauline@gmail .com>
2023-02-20 18:10:57 -0800

    ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - Entry point
+ *
+ * Result: Always 0 (success)
+ */
+
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+
+}
+
